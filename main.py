d9cea7a28603be9f19f47091a3cbf75af4dc36e3
joao pedro <joao.albuquerque@irede.org.br>
2023-08-11 16:47:25 -0300

    setting

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+from bs4 import BeautifulSoup as bs
+
+from selenium import webdriver
+
+class PriceSpy:
+    self.browser = webdriver.Chrome()
+
+if __name__ == '__main__':
+    pass
